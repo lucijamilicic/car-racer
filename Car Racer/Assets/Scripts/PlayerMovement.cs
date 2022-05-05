bfb4f9947ca17923016e101d24c69cdcc1bcca58
lucijamilicic <mi18029@alas.matf.bg.ac.rs>
2022-05-05 20:18:04 +0200

    Add car assets and basic movement

---
+++ Car Racer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 direction = new Vector3(horizontal * 0.05f, 0f, speed);
+        transform.Translate(direction);
+        
+    }
+}
